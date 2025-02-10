ca2ff975e529420d897db764be22134b01379a0a
hendisantika <hendisantika@yahoo.co.id>
2025-02-10 13:52:29 +0700

    fix: Spotless :three:🫸🌀✏️📗🐧🐳⬆

--- src/test/java/id/my/hendisantika/AppTest.java
+++ src/test/java/id/my/hendisantika/AppTest.java
@@ -15,51 +15,53 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
  * 13.13 To change this template use File | Settings | File Templates.
  */
 public class AppTest {
-  @BeforeAll
-  static void setup() {
-    System.out.println("@BeforeAll - executes once before all test methods in this class");
-  }
+	@BeforeAll
+	static void setup() {
+		System.out.println("@BeforeAll - executes once before all test methods in this class");
+	}
 
-  @BeforeEach
-  void init() {
-    System.out.println("@BeforeEach - executes before each test method in this class");
-  }
+	@BeforeEach
+	void init() {
+		System.out.println
+				("@BeforeEach - executes before each test method in this class")
+		;
+	}
 
-  @DisplayName("Single test successful")
-  @org.junit.jupiter.api.Test
-  void testSingleSuccessTest() {
-    System.out.println("Success");
-  }
+	@DisplayName("Single test successful")
+	@org.junit.jupiter.api.Test
+	void testSingleSuccessTest() {
+		System.out.println("Success");
+	}
 
-  @org.junit.jupiter.api.Test
-  @Disabled("Not implemented yet")
-  void testShowSomething() {
-    System.out.println("testShowSomething");
-  }
+	@org.junit.jupiter.api.Test
+	@Disabled("Not implemented yet")
+	void testShowSomething() {
+		System.out.println("testShowSomething");
+	}
 
-  @org.junit.jupiter.api.Test
-  void groupAssertions() {
-    int[] numbers = {0, 1, 2, 3, 4};
-    assertAll(
-            "numbers",
-            () -> assertEquals(numbers[1], 1),
-            () -> assertEquals(numbers[3], 3),
-            () -> assertEquals(numbers[2], 2));
-  }
+	@org.junit.jupiter.api.Test
+	void groupAssertions() {
+		int[] numbers = {0, 1, 2, 3, 4};
+		assertAll(
+				"numbers",
+				() -> assertEquals(numbers[1], 1),
+				() -> assertEquals(numbers[3], 3),
+				() -> assertEquals(numbers[2], 2));
+	}
 
-  @org.junit.jupiter.api.Test
-  void shouldThrowException() {
-    Throwable exception = assertThrows(UnsupportedOperationException.class, () -> {
-      throw new UnsupportedOperationException("Not supported");
-    });
-    assertEquals("Not supported", exception.getMessage());
-  }
+	@org.junit.jupiter.api.Test
+	void shouldThrowException() {
+		Throwable exception = assertThrows(UnsupportedOperationException.class, () -> {
+			throw new UnsupportedOperationException("Not supported");
+		});
+		assertEquals("Not supported", exception.getMessage());
+	}
 
-  @org.junit.jupiter.api.Test
-  void assertThrowsException() {
-    String str = null;
-    assertThrows(IllegalArgumentException.class, () -> {
-      Integer.valueOf(str);
-    });
-  }
+	@org.junit.jupiter.api.Test
+	void assertThrowsException() {
+		String str = null;
+		assertThrows(IllegalArgumentException.class, () -> {
+			Integer.valueOf(str);
+		});
+	}
 }
